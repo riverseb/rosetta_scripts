d8d3c65006f9a9d9c0c08242ebe26e89c7edd3b2
riverseb <seb.rivera.enzymes@gmail.com>
2024-03-18 14:48:52 -0400

    Fix issue #1: added try block to calc_rmsd_atomSubset to suppress segfault from failure to find substructure match

--- src/python/calcRMSD_atomSubset.py
+++ src/python/calcRMSD_atomSubset.py
@@ -65,12 +65,24 @@ def calc_rmsd_atomSubset(aligned_ref_ligand, query_ligand):
     :param query_ligand: query ligand pdb file
     """
     # load the reference ligand
+    print("Loading reference ligand...")
     ref_mol = rd.rdmolfiles.MolFromMol2File(aligned_ref_ligand)
+    print("Loading query ligand...")
     query_mol = rd.rdmolfiles.MolFromMol2File(query_ligand)
+    print("Performing substructure matching for reference ligand...")
     ref_match = ref_mol.GetSubstructMatch(query_mol)
+    print("Performing substructure matching for query ligand...")
     query_match = query_mol.GetSubstructMatch(ref_mol)
-    rmsd = AllChem.CalcRMS(ref_mol, query_mol, map=[list(zip(query_match , ref_match))]) 
-    return rmsd
+    print("Calculating RMSD...")
+    # rmsd = AllChem.CalcRMS(ref_mol, query_mol, map=[list(zip(query_match , ref_match))])
+    try:
+        rmsd = rdMolAlign.CalcRMS(ref_mol, query_mol)
+        return rmsd
+    except:
+        print(f"RMSD calculation failed for {query_ligand}")
+        rmsd = None
+        return rmsd  
+    
 
 def main(ref_pdb, query_pdb_dir, ref_ligand, query_ligand):
     print("Starting PyMOL...")
@@ -89,6 +101,7 @@ def main(ref_pdb, query_pdb_dir, ref_ligand, query_ligand):
     if not os.path.exists("scores/"): os.mkdir("scores/")
     ref_name = ref_pdb.split("/")[-1].split(".")[0]
     # create rmsd vs score table for each query ligand
+    print("Extracting query ligands...")
     with open(f"scores/rmsd_scores_{ref_name}.txt", "w") as out:
         out.seek(0)
         out.write("rmsd\tquery_ligand\tdescription\n")
@@ -99,11 +112,15 @@ def main(ref_pdb, query_pdb_dir, ref_ligand, query_ligand):
             index_list = query.split(".")[0].split("_")[-2:]
             index = "_".join(index_list)
             # extract and save query ligand
+            print(f"Extracting query ligand for {query}...")
             extract_and_save_query_ligand(query_pdb, query_ligand, index)
             # calculate rmsd
+            print(f"Calculating rmsd for {query}...")
             rmsd = calc_rmsd_atomSubset(f"aligned_{ref_ligand}.mol2", f"docked_ligands/{query_ligand}_{index}.mol2")
             # add to rmsd table
-            rmsd_table.append([rmsd, query_ligand, query])
+            if rmsd:
+                print(f"Adding {query} to rmsd table...")
+                rmsd_table.append([rmsd, query_ligand, query])
         # sort by rmsd
         sorted_rmsd_table = sorted(rmsd_table, key=lambda x: float(x[0]))
         # write out rmsd vs score table
