55b684b3691794ad491a199c94130d5653609195
riverseb <seb.rivera.enzymes@gmail.com>
2024-04-17 18:13:54 -0400

    reworked option handling. condensed output values into dict and returned

--- src/python/postGALD_process.py
+++ src/python/postGALD_process.py
@@ -3,11 +3,13 @@ import argparse
 from argparse import ArgumentError
 import GALD_file_clean as file_clean
 import calcRMSD_atomSubset as rmsd_calc
-import rmsd_vs_score as rmsd_plot 
+import rmsd_vs_score as rmsd_plot
+import os
+import warnings
 
 def main(project, inputFile, repeats=5, native=None, rmsd=True, query_pdbs="pdbs/", 
          query_lig=None, ref_lig=None, clean=True, rmsd_path=None,
-         score_path="scores/fullscore_sorted.sc"):
+         score_path="scores/fullscore_sorted.sc", overwrite=False):
     """ 
     Cleans the GALigandDock output files and generates rmsd vs score plots
 
@@ -25,33 +27,40 @@ def main(project, inputFile, repeats=5, native=None, rmsd=True, query_pdbs="pdbs
     if clean:
         print("Cleaning files...")
         file_clean.main(project, inputFile, repeats)
-    if rmsd and native:
+    
+    ref_name = native.split("/")[-1].split(".")[0]
+
+    if not rmsd_path:
+        rmsd_path = f"scores/rmsd_scores_{ref_name}.txt"
+    
+    if rmsd:
         if query_lig and ref_lig:
             print("Calculating rmsd...")
-            rmsd_calc.main(native, query_pdbs, ref_lig, query_lig)
-        elif not query_lig and not ref_lig:
-            ref_parsed_lig = native.strip(".pdb").split("_")[-1]
-            query_parsed_lig = inputFile.strip(".pdb").split("_")[-1]
-            rmsd_calc.main(native, query_pdbs, ref_parsed_lig, query_parsed_lig)
-        elif query_lig and not ref_lig:
-            ref_parsed_lig = native.strip(".pdb").split("_")[-1]
-            rmsd_calc.main(native, query_pdbs, ref_lig, query_parsed_lig)
-        elif not query_lig and ref_lig:
-            query_parsed_lig = inputFile.strip(".pdb").split("_")[-1]
-            rmsd_calc.main(native, query_pdbs, ref_parsed_lig, query_lig)
-        ref_name = native.split("/")[-1].split(".")[0]
-        
-        if not rmsd_path:
-            rmsd_path = f"scores/rmsd_scores_{ref_name}.txt"
-        print("Plotting rmsd vs score...", flush=True)
-        pnear, CI, best_model =rmsd_plot.main(rmsd_path, ref_name, score_path,
-                                              outfile="PNear.txt")
-        print(f"PNear: {pnear:.2f} (CI 95%:[{CI[0]:.3f} - {CI[1]:.3f}]), Best Model: {best_model}")
-        return pnear, CI, best_model
-    elif rmsd and not native:
-        raise ArgumentError("Must provide native pdb file for rmsd calculation.")
+            if not os.path.exists(rmsd_path) or overwrite or os.stat(rmsd_path).st_size == 0:
+                rmsd_calc.main(native, query_pdbs, ref_lig, query_lig)
+            else:
+                warnings.warn(message=f"RMSD file for {ref_name} already exisits. \
+                              To overwrite, provide --overwrite arg.")
+
+        else:
+            raise ArgumentError(message="ERROR: Must provide query and reference residue name")   
     
+    if not native:
+        raise ArgumentError(message="ERROR: Must provide reference structure")
+    print("Plotting rmsd vs score...", flush=True)        
+    pnear, CI, best_model =rmsd_plot.main(rmsd_path, ref_name, score_path,
+                                            outfile="PNear.txt")
+    print(f"PNear: {pnear:.2f} (CI 95%:[{CI[0]:.3f} - {CI[1]:.3f}]), Best Model: {best_model}")
     
+    results = {
+    'pnear': pnear,
+    'CI': CI,
+    'best_rmsd': best_model['rmsd'],
+    'total_score': best_model['total_score'],
+    'best_model': best_model['description']
+    }
+
+    return results
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -66,6 +75,7 @@ if __name__ == "__main__":
     parser.add_argument("--no_clean", action="store_false", dest="clean")
     parser.add_argument("--rmsd_path", type=str, default=None)
     parser.add_argument("--score_path", type=str, default="scores/fullscore_sorted.sc")
+    parser.add_argument("--overwrite", action="store_true")
     args = parser.parse_args()
     main(**vars(args))
     
